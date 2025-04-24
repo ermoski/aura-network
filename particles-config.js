1aa6e4dfabffcf53f94e763e2a2e6ed4fdb4b424
ermoski <ermoskito25@gmail.com>
2025-04-24 19:09:56 +0200

    Add files via upload

--- particles-config.js
+++ particles-config.js
@@ -1,40 +1,14 @@
-
-particlesJS('particles-js',
-  {
-    "particles": {
-      "number": {
-        "value": 90,
-        "density": {
-          "enable": true,
-          "value_area": 700
-        }
-      },
-      "color": {
-        "value": "#FFA500"
-      },
-      "shape": {
-        "type": "circle"
-      },
-      "opacity": {
-        "value": 0.6,
-        "random": true
-      },
-      "size": {
-        "value": 3,
-        "random": true
-      },
-      "line_linked": {
-        "enable": false
-      },
-      "move": {
-        "enable": true,
-        "speed": 1.8,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out"
-      }
-    },
-    "retina_detect": true
-  }
-)
+particlesJS('particles-js', {
+  particles: {
+    number: { value: 60, density: { enable: true, value_area: 800 } },
+    color: { value: '#FFA500' },
+    shape: { type: 'circle' },
+    opacity: { value: 0.7, random: true },
+    size: { value: 4, random: true },
+    move: { enable: true, speed: 1.5, random: true, out_mode: 'out' }
+  },
+  interactivity: {
+    events: { onhover: { enable: true, mode: 'repulse' } }
+  },
+  retina_detect: true
+});
