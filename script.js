926e802370793d9e44565f9593a83728d7546d10
ermoski <ermoskito25@gmail.com>
2025-04-24 11:18:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,19 +10,19 @@ function resize() {
 window.addEventListener('resize', resize);
 resize();
 
-for (let i = 0; i < 120; i++) {
+for (let i = 0; i < 100; i++) {
   particles.push({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
     r: Math.random() * 1.5 + 0.5,
-    dx: (Math.random() - 0.5) * 0.4,
-    dy: (Math.random() - 0.5) * 0.4
+    dx: (Math.random() - 0.5) * 0.6,
+    dy: (Math.random() - 0.5) * 0.6
   });
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "rgba(255,140,0,0.06)";
+  ctx.fillStyle = "rgba(255,140,0,0.05)";
   particles.forEach(p => {
     ctx.beginPath();
     ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
