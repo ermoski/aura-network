d76ef9f961833202b21b87113dabb85023f89cab
ermoski <ermoskito25@gmail.com>
2025-04-24 17:18:25 +0200

    Add files via upload

--- particles-config.js
+++ particles-config.js
@@ -1,11 +1,11 @@
 particlesJS('particles-js', {
   particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
-    color: { value: '#ff6a00' },
+    number: { value: 60, density: { enable: true, value_area: 800 } },
+    color: { value: '#FFA500' },
     shape: { type: 'circle' },
-    opacity: { value: 0.5, random: true },
-    size: { value: 3, random: true },
-    move: { enable: true, speed: 2, random: true, out_mode: 'out' }
+    opacity: { value: 0.7, random: true },
+    size: { value: 4, random: true },
+    move: { enable: true, speed: 1.5, random: true, out_mode: 'out' }
   },
   interactivity: {
     events: { onhover: { enable: true, mode: 'repulse' } }
